ca85a371567f1936f073cf0fa10136b918e9a448
Matt Turnshek <matthewturnshek@gmail.com>
2017-05-16 11:22:58 -0400

    updated dqn with double-q learning and batch updates

--- reinforcement learning/keras-openaigym.py
+++ reinforcement learning/keras-openaigym.py
@@ -1,3 +1,10 @@
+# DQN for OpenAI Gym environments
+#
+# additions:
+# - batch updates
+# - prioritized experience replay 	(not done)
+# - Double Q network
+
 import keras
 from keras.models import Sequential
 from keras.layers import Conv2D, Dense, Activation, Flatten, Dropout, BatchNormalization
@@ -7,20 +14,21 @@ import numpy as np
 
 import gym
 
+import random # not permanent ....
+
 ###########################
 ### Model Configuration ### 
 ###########################
 
-def simpleNN(num_actions, state_shape):
+def simple(num_actions, state_shape):
 	model = Sequential()
 
-	model.add(Dense(32, input_shape=state_shape, activation='relu'))
-	model.add(Dense(32, activation='relu'))
+	model.add(Dense(64, input_shape=state_shape, activation='relu'))
 
 	# Max of this will be the action we take
 	model.add(Dense(num_actions, activation='linear'))
 
-	optimizer = rmsprop(lr=0.0002, decay=1e-6)
+	optimizer = rmsprop(lr=0.00025, decay=1e-6)
 	model.compile(optimizer=optimizer, loss='mse')
 
 	model.summary()
@@ -32,7 +40,7 @@ def simpleNN(num_actions, state_shape):
 ############################################
 
 class Agent():
-	def __init__(self, game_string, model_fn, done_reward=0, max_steps_per_episode=2000):
+	def __init__(self, game_string, model_fn, done_reward=0, max_steps_per_episode=5000):
 		self.env = gym.make(game_string)
 		self.num_actions = self.env.action_space.n
 		self.state_shape = self.env.reset().shape
@@ -40,76 +48,94 @@ class Agent():
 		self.done_reward = done_reward
 
 		self.model = model_fn(self.num_actions, self.state_shape)
+		self.model2 = model_fn(self.num_actions, self.state_shape) # double q network
 		self.replays = []
-		self.replay_decay_size = 2048
-		self.batch_size = 32
+		self.batch_size = 64
 
 		# q-learning parameters
 		self.epsilon = 1
 		self.min_epsilon = .01
-		self.epsilon_decay = lambda x: x - .001
+		self.epsilon_decay = lambda x: x*.995
 		self.gamma = .99	# long-term vs short-term reward
 
-	def add_replay(self, state, action, reward, state_next, done):
-		self.replays.append((state, action, reward, state_next, done))
+	#################
+	# Model methods #
+	#################
 
-	# halves replays at random ... meant to weight more recent memories more highly
-	def replay_decay(self):
-		kept_indices = np.random.choice(len(self.replays), len(self.replays)//2, replace=False)
-		kept_replays = []
-		for replay_index in kept_indices:
-			kept_replays.append(self.replays[replay_index])
-		self.replays = kept_replays
+	def predictOne(self, state, dqn_n=1):
+		if dqn_n == 1:
+			return self.model.predict(np.array([state]))[0]
+		else:
+			return self.model2.predict(np.array([state]))[0]
+
+	#############################
+	# Experience replay methods #
+	#############################
+
+	def add_replay(self, state, action, reward, state_next, done):
+		self.replays.append([state, action, reward, state_next, done])
+
+	#######################
+	# Acting and Learning #
+	#######################
 
 	def get_action(self, state):
 		if np.random.rand() < self.epsilon:
 			action = self.env.action_space.sample()
 		else:
-			action = np.argmax(self.model.predict(state)[0])
+			action = np.argmax(self.predictOne(state))
 		return action
 
+
 	def learn(self):
+		# randomly either learn on the first or second model
+		if np.random.rand() < .5:
+			print('1')
+			m1 = self.model
+			m2 = self.model2
+		else:
+			print('2')
+			m1 = self.model2
+			m2 = self.model
+
 		batch_size = min(self.batch_size, len(self.replays))
-		replay_indices = np.random.choice(len(self.replays), batch_size)
+		random.shuffle(self.replays)
+		replay_batch = self.replays[:batch_size]
 
-		states = []
-		target_fs = []
+		no_state = np.zeros(self.state_shape)
+ 
+		s = np.array([ replay[0] for replay in replay_batch ])
+		s_ = np.array([ (no_state if replay[3] is None else replay[3]) for replay in replay_batch ])
+		
+		p = m1.predict(s)
+		p_ = m1.predict(s_)
+		p2_ = m2.predict(s_)
 
-		for replay_index in replay_indices:
-			state, action, reward, state_next, done = self.replays[replay_index]
-			if done:
-				target = reward
-			else:
-				next_prediction = np.amax(self.model.predict(state_next)[0])
-				target = reward + self.gamma*(next_prediction)
+		for i in range(len(replay_batch)):
+			_, action, reward, _, done = self.replays[i]
 
-			target_f = self.model.predict(state)
-			target_f[0][action] = target
+			target = reward
+			if not done:
+				max_value_action = np.argmax(p_[i])
+				target += self.gamma*p2_[i][max_value_action]
 
-			states.append(state[0])
-			target_fs.append(target_f[0])
+			p[i][action] = target
 
-		self.model.fit(np.array(states), np.array(target_fs), epochs=1, verbose=0)
+		m1.fit(s, p, epochs=1, verbose=0)
+
 
 	def one_episode(self, episode_number, total_episodes):
 		state = self.env.reset()
-		state = np.array([state])
 
 		for t in range(self.max_steps_per_episode):
 			self.env.render()
 
 			action = self.get_action(state)
 			state_next, reward, done, info = self.env.step(action)
-			state_next = np.array([state_next])
 
-			if done:
-				reward += self.done_reward
-			
+			if done: reward += self.done_reward
+
 			self.add_replay(state, action, reward, state_next, done)
-
-			if len(self.replays) >= self.replay_decay_size:
-				self.replay_decay()
-
 			self.learn()
 
 			state = state_next
@@ -118,7 +144,8 @@ class Agent():
 
 		print("episode: {}/{}, score: {}, epsilon: {}".format(episode_number, total_episodes, t, self.epsilon))
 
-	def play(self, episodes=1000):
+
+	def play(self, episodes=2000):
 		for episode in range(episodes):
 			self.one_episode(episode_number=episode, total_episodes=episodes)
 			if self.epsilon > self.min_epsilon:
@@ -128,6 +155,13 @@ class Agent():
 ### Create and run learner ###
 ##############################
 
-agent = Agent('CartPole-v0', simpleNN, done_reward=-100)
+gym.envs.register(
+    id='CartPole-v2',
+    entry_point='gym.envs.classic_control:CartPoleEnv',
+    tags={'wrapper_config.TimeLimit.max_episode_steps': 5000},
+    reward_threshold=195.0,
+)
+
+agent = Agent('CartPole-v2', simple)
 
 agent.play()
